a49f88abb07b543779363e5f3130e3b9bbaf5a5a
nataraj <109903331+leocoderz@users.noreply.github.com>
2023-08-01 12:16:38 +0530

    Add files via upload

---
+++ Query/src/main/java/com/example/Query/service/QueryService.java
@@ -0,0 +1,38 @@
+package com.example.Query.service; 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Query.model.Queries;
+import com.example.Query.repository.QueryRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+
+public class QueryService {
+
+    @Autowired
+    QueryRepository queryRepository;
+
+    public List<Queries> getTableDatas() {
+        return queryRepository.findAllData();
+    }
+    
+    public List<Queries> getDetail(Boolean activeState, List<String> descriptionList){
+        return queryRepository.findData(activeState, descriptionList);
+    }
+
+    public List<Queries> getTableData() {
+        return queryRepository.findAlldetail();
+    }
+
+    public int updateDatas(Boolean activeState, List<Long> idList) {
+        return queryRepository.updateData(activeState, idList);
+    }
+
+
+}
